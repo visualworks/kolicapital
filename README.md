b14f0d4fd80e6c87dcd7340f23e0d44f01ad1880
Henrique Mattos <henrique.mattos@sap.com>
2020-01-14 23:31:15 +0100

    Removed old webpack project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# kolicapital
+# Köli Capital
+
+A Köli Capital está em fase de obtenção de credenciamento perante a Comissão de Valores Mobiliários (CVM). A atividade de gestão de recursos somente será iniciada após a devida habilitação.
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "scss/style.scss";
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-import Layout from "components/layout";
-
-
-const layout = <Layout />;
-const targetDiv = document.getElementById("app");
-ReactDOM.render(layout, targetDiv);
